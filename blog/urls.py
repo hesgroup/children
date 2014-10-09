f16733ceae1dfe8c90c05223eba8fcfe98dadc59
milad283 <milad283@gmail.com>
2014-10-10 02:24:39 +0330

    final

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,4 @@ from blog.models import Post
 
 urlpatterns = patterns('',
     url(r'^$',ListView.as_view(queryset = Post.objects.all().order_by("-date")[:5],template_name="blog.html")),
-    url(r'^(?P<pk>\d+)$',DetailView.as_view(model=Post,template_name="post.html")),
 )
--- child/urls.py
+++ child/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import ListView,DetailView
+from blog.models import Post
 
 urlpatterns = patterns('',
-	url(r'^(?i)blog/', include('blog.urls')),
+	url(r'^$', include('blog.urls')),
+    url(r'^(?P<pk>\d+)$',DetailView.as_view(model=Post,template_name="post.html")),
 	url(r'^about/', include('about.urls')),
 	url(r'^(?i)payment/', include('payment.urls', namespace='payment')),
 	url(r'^admin/', include(admin.site.urls)),
